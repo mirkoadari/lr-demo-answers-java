57e2373aa5e1469010c1ddad9259d281e117e4a0
Geert Bevin <gbevin@uwyn.com>
2013-11-04 11:51:06 +0200

    Fixed test script to adapt to different ruby locations on different vagrant boxes

--- webapp/src/main/webapp/WEB-INF/liverebel/scripts/appUp/always/run_tests.sh
+++ webapp/src/main/webapp/WEB-INF/liverebel/scripts/appUp/always/run_tests.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# adapt to different locations of vagrant boxes ruby locations
+if [ -e /opt/vagrant_ruby/bin/ruby ]; then ruby='/opt/vagrant_ruby/bin/ruby'; fi
+if [ -e /opt/ruby/bin/ruby ]; then ruby='/opt/ruby/bin/ruby'; fi
+if [ -z "$ruby" ]; then
+	echo "Not able to find a known ruby executable"
+	exit 1
+fi
+
 remoteport=$(cat "$HOME/tunnelport")
-port=$( echo "$directUrls" | /opt/ruby/bin/ruby -ruri -e 'puts URI.parse(gets.chomp).port' )
+port=$( echo "$directUrls" | $ruby -ruri -e 'puts URI.parse(gets.chomp).port' )
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -nNT -R $remoteport:localhost:$port vagrant@java.answers.liverebel.com &
 pid=$!
 status=$?
